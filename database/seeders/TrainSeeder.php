02819111ab5f557759b6280a3c968e302602e2d3
Skyfe24 <fabio.erase@gmail.com>
2023-08-25 16:31:51 +0200

    Trainseeder call in DatabaseSeeder

---
+++ database/seeders/TrainSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Train;
+
+class TrainSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $new_train = new Train();
+
+        $new_train-> 
+
+        $new_train-> 
+        
+        $new_train-> 
+
+        $new_train-> 
+
+        $new_train-> 
+
+        $new_train-> 
+
+        $new_train-> 
+
+        $new_train-> 
+
+        $new_train-> 
+
+        $new_train-> 
+
+        $new_train-> 
+
+        $new_train-> 
+        
+        
+    }
+}
